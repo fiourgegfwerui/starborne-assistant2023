7bb63db0e5bceed267d9917d4d6356fb748360d6
Oscar roberts <129421284+fiourgegfwerui@users.noreply.github.com>
2023-08-19 10:20:39 -0700

    maybe fixed auth?

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,7 +8,7 @@ export default {
 	PATCHKIT_CLIENT_SECRET: "ed6557e211c93919d637b61e7278fbcf",
 	VERSION_VALUE: "0.1.1.1650",
 	ENDPOINTS: {
-		AUTH: "https://sba.starborne.com/Token",
+		AUTH: "52.169.105.170",
 		LIST_GAMES: "https://sba.starborne.com/api/Player/GetGames",
 		JOIN_SERVER: "https://sba.starborne.com/api/Player/JoinServer"
 	},
